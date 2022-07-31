be1dc72e7ae9b043d81e73e290ba96b5698b45a4
mases777 <mases@abv.bg>
2022-07-31 11:23:39 +0300

    M10-Edit8

---
+++ 2021-2022-M10/2021-10-31-Izpit-online-M10/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static List<string> connections = new List<string>();
+        static List<string> insides = new List<string>();
+        static List<string> old = new List<string>();
+        static List<string> foundConnections = new List<string>();
+        static List<string> allConnections = new List<string>();
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            while (line != "end")
+            {
+                connections.Add(line.Remove(1, 1));
+                string inner = line.Split(" ")[1];
+                if (!allConnections.Contains(line.Split(" ")[0])) allConnections.Add(line.Split(" ")[0]);
+                if (!allConnections.Contains(inner)) allConnections.Add(inner);
+                if (!insides.Contains(inner))
+                    insides.Add(inner);
+                line = Console.ReadLine();
+            }
+
+            for (int i = 0; i < insides.Count; i++)
+            {
+                for (int j = 0; j < connections.Count; j++)
+                {
+                    if (connections[j][1].ToString() == insides[i])
+                    {
+                        string reversed = connections[j][1] + "" + connections[j][0];
+                        string normal = connections[j][0] + "" + connections[j][1];
+                        if (insides.Contains(connections[j][0].ToString()) && connections.Contains(reversed) && !old.Contains(normal))
+                        {
+                            old.Add(reversed);
+                            foundConnections.Add(insides[i] + " <-> " + connections[j][0].ToString());
+                        }
+                    }
+                }
+            }
+
+            if (foundConnections.Count == 0)
+            {
+                Console.WriteLine("Disconnected");
+            }
+            else
+                foreach (var all in allConnections)
+                {
+                    foreach (string item in foundConnections.OrderBy(x => x))
+                    {
+                        if (all.Contains(item[0]))
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                }
+        }
+    }
+}
---
+++ 2021-2022-M10/2021-10-31-Izpit-online-M10/Task2/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static bool hasPerm = false;
+        static List<string> results = new List<string>();
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string[] perm = new string[input.Length];
+            bool[] used = new bool[input.Length];
+            List<string> strings = new List<string>();
+            string line = Console.ReadLine();
+            while (line != "end")
+            {
+                strings.Add(line);
+                line = Console.ReadLine();
+            }
+
+            Gen(0, input, perm, used, strings);
+            if (!hasPerm)
+            {
+                Console.WriteLine("No permutations...");
+            }
+            foreach (string item in results.OrderBy(x => x))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static void Gen(int index, string[] str, string[] perm, bool[] used, List<string> strings)
+        {
+            if (index >= str.Length)
+            {
+                if (strings.Contains(string.Join("", perm)))
+                {
+                    results.Add(string.Join(" ", perm));
+                    hasPerm = true;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        perm[index] = str[i];
+                        Gen(index + 1, str, perm, used, strings);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+//class Program
+//{
+//    static List<char> solutionList = new List<char>();
+//    static void Main(string[] args)
+//    {
+//        var input = Console.ReadLine().Split().ToArray();
+//        char[] ch = new char[input.Count()];
+//        for (int i = 0; i < ch.Length; i++)
+//        {
+//            ch[i] = char.Parse(input[i]);
+//        }
+//        string line = Console.ReadLine();
+//        int br = 0;
+//        Permute(ch[0], ch[ch.Length - 1]);
+
+//        //while (line != "end")
+//        //{
+//        //    if (Perm(ch, line))
+//        //    {
+//        //        Print(ch, line); br++;
+//        //    }
+//        //    line = Console.ReadLine();
+//        //}
+//        //if (br == 0) Console.WriteLine("No permutations…");
+//    }
+
+//    //public static bool Perm(char[] a, string line)
+//    //{
+//    //    int j, pom, i = line.Length;
+//    //    while (a[i] < a[i - 1]) i--;
+//    //    j = i; i--;
+//    //    if (i == 0) return false;
+//    //    while (a[j] > a[i]) j++;
+//    //    pom = a[i]; a[i] = a[j - 1]; a[j - 1] = pom;
+//    //    for (j = i + 1; j <= (i + 1 + n) / 2; j++)
+//    //    {
+//    //        pom = a[j];
+//    //        a[j] = a[i + 1 + n - j];
+//    //        a[i + 1 + n - j] = pom;
+//    //    }
+//    //    return true;
+//    //}
+//    public static void Print(char[] a, string line)
+//    {
+//        for (int i = 1; i <= line.Length; i++)
+//        {
+//            Console.Write(a[i] + " ");
+//        }
+//        Console.WriteLine();
+//    }
+
+//    static List<char> currentSolution = new List<char>();
+
+//    private static void Permute(char ind, char n)
+//    {
+//        if (currentSolution.Count == (n-ind))
+//        {
+//            solutionList = currentSolution;
+//            //for (int i = 0; i < n; i++)
+//            //{
+//            //    Console.Write("{0,2}", currentSolution[i]);
+//            //}
+//            Console.WriteLine();
+//            return;
+//        }
+
+//        for (char i = ind; i <= n; i++)
+//        {
+//            if (!currentSolution.Contains(i))
+//            {
+//                currentSolution.Add(i);
+//                Permute(ind++, n);
+//                currentSolution.Remove(i);
+//            }
+//        }
+
+//    }
+
+
+
+//    //static void Main(string[] args)
+//    //{
+//    //    const int max = 9; int n;
+//    //    do
+//    //    {
+//    //        n = int.Parse(Console.ReadLine());
+
+//    //    } while (n < 1 || n > max);
+//    //    int[] a = new int[n + 2];
+//    //    for (int i = 1; i <= n; i++)
+
+//    //        a[i] = i;
+//    //    a[0] = a[n + 1] = 0;
+//    //    do Print(a, n); while (Perm(a, n));
+//    //    Console.ReadKey(true);
+//    //}
+//    //public static void Print(int[] a, int n)
+//    //{
+//    //    for (int i = 1; i <= n; i++)
+//    //    {
+//    //        Console.Write(a[i] + " ");
+//    //    }
+//    //    Console.WriteLine();
+//    //}
+//    //public static bool Perm(int[] a, int n)
+//    //{
+//    //    int j, pom, i = n;
+//    //    while (a[i] < a[i - 1]) i--;
+//    //    j = i; i--;
+//    //    if (i == 0) return false;
+//    //    while (a[j] > a[i]) j++;
+//    //    pom = a[i]; a[i] = a[j - 1]; a[j - 1] = pom;
+//    //    for (j = i + 1; j <= (i + 1 + n) / 2; j++)
+//    //    {
+//    //        pom = a[j];
+//    //        a[j] = a[i + 1 + n - j];
+//    //        a[i + 1 + n - j] = pom;
+//    //    }
+//    //    return true;
+
+//    //}
+//}
+
