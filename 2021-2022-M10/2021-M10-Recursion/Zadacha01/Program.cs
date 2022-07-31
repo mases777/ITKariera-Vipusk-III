3acdd0e137d3d2ff019dcfc7352bea32288cea14
mases777 <mases@abv.bg>
2022-07-31 11:12:25 +0300

    M10

---
+++ 2021-2022-M10/2021-M10-Recursion/Zadacha01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(Gcd(a, b));
+            Console.WriteLine(Lcm(a, b));
+        }
+
+        private static int Gcd(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            if (a > b)
+            {
+                return Gcd(a - b, b);
+            }
+            else
+            {
+                return Gcd(a, b - a);
+            }
+        }
+
+        private static int Lcm(int a, int b)
+        {
+            return (a * b) / Gcd(a, b);
+        }
+    }
+}
