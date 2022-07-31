ae0533c28d494428d4dfa1ff79fde61fb6691bb1
mases777 <mases@abv.bg>
2022-07-31 11:16:51 +0300

    M10-Edit3

---
+++ 2021-2022-M10/2021-M10-Combinatorics/Zadacha01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    class Program
+    {
+        const int n = 5;
+        static int[] elements = new int[] { 0, 1, 2, 3, 4 };
+        static bool[] used = new bool[n];
+        static int[] perm = new int[n];
+        static void Main(string[] args)
+        {
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", perm));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        perm[index] = elements[i];
+                        Permute(index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2021-2022-M10/2021-M10-Combinatorics/Zadacha02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace Zadacha02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
