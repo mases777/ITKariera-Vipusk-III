5607322a4e9dbeb5b952a4cc4d1b8969896a9fb7
mases777 <mases@abv.bg>
2022-07-31 11:18:57 +0300

    M10-Edit5

---
+++ 2021-2022-M10/2021-M10-Trees/Zadacha01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Zadacha01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>
+                (
+                    7,
+                    new Tree<int>
+                    (
+                        19,
+                        new Tree<int>(1),
+                        new Tree<int>(12),
+                        new Tree<int>(31)
+                    ),
+                    new Tree<int>(21),
+                    new Tree<int>
+                    (
+                        14,
+                        new Tree<int>(23),
+                        new Tree<int>(6)
+                    )
+                );
+
+            tree.Print();
+
+            var ordered = tree.OrderDFS();
+            Console.WriteLine($"DFS: {string.Join(" ", ordered)}");
+
+            var ordered2 = tree.BFS();
+            Console.WriteLine($"BFS: {string.Join(" ", ordered2)}");
+        }
+    }
+}
---
+++ 2021-2022-M10/2021-M10-Trees/Zadacha01/Tree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    public class Tree<T>
+    {
+        private T value;
+        private IList<Tree<T>> children;
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.value = value;
+            this.children = children;
+        }
+
+        public void Print(int indent = 0)
+        {
+            Console.Write(new string(' ', 2 * indent));
+            Console.WriteLine(this.value);
+            foreach (var item in this.children)
+            {
+                item.Print(indent + 1);
+            }
+        }
+
+        private void DFS(Tree<T> tree, List<T> order)
+        {
+            foreach (var item in tree.children)
+            {
+                this.DFS(item, order);
+            }
+            order.Add(tree.value);
+        }
+        public IEnumerable<T> OrderDFS()
+        {
+            List<T> order = new List<T>();
+            this.DFS(this, order);
+            return order;
+        }
+
+        public IEnumerable<T> BFS()
+        {
+            List<T> order = new List<T>();
+            Queue<Tree<T>> visited = new Queue<Tree<T>>();
+            visited.Enqueue(this);
+            while (visited.Count > 0)
+            {
+                var next = visited.Dequeue();
+                order.Add(next.value);
+                foreach (var item in next.children)
+                {
+                    visited.Enqueue(item);
+                }
+            }
+            return order;
+        }
+    }
+}
